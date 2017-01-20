5bc411a9fb37aafbf50ff944b6d1b00b243c30ab
JamesRobertsonGames <i7291351@bournemouth.ac.uk>
2017-01-20 22:55:13 +0000

    Added simple turn logic

---
+++ Assets/Scripts/Turns.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turns : MonoBehaviour
+{
+    public int PlayerTurnSelect;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	
+	}
+
+    void TurnSelect ()
+    {
+        switch(PlayerTurnSelect)
+        {
+            case 1:
+                // Temp - put player rounds in here
+                int o = 0;
+                break;
+        }
+    }
+}
