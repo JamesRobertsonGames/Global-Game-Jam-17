872155db1b963417251892a58cc3ad3a17d32628
unknown <i7291351@PC34211.student.bournemouth.ac.uk>
2017-01-20 20:58:57 +0000

    Compiler Error Fixed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    public GameObject sample;
+
+    // Use this for initialization
+    void Start ()
     {
         // This is completely a test to see if this is possible
-        public GameObject sample;	
+        
 	}
 	
 	// Update is called once per frame
