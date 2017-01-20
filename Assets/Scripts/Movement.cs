1f9b8e6b226a7ca20d435a2380b83968a075489e
JamesRobertsonGames <i7291351@bournemouth.ac.uk>
2017-01-20 22:46:24 +0000

    Added dice roll functionality

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    public GameObject sample;
+    public GameObject boat;
+    public float MovementAmount;
+    public GameObject GameWorldWater;
+
+    public bool isPlayerActive;
 
     // Use this for initialization
     void Start ()
     {
+        
         // This is completely a test to see if this is possible
         
 	}
