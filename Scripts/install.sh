9961c622b9e157be7c8b64b3cb7cb7eaf5b77bb9
unknown <i7754737@PC34214.student.bournemouth.ac.uk>
2017-01-21 19:39:19 +0000

    gave up

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,16 +1,29 @@
 #! /bin/sh
 
-# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=e87ab445ead0
+VERSION=5.3.2f1
 
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-unity_url="http://download.unity3d.com/download_unity/960ebf59018a/UnityDownloadAssistant-5.3.5f1.dmg"
-echo "Downloading from $unity_url: "
-curl -o Unity.dmg "$unity_url"
-hdiutil attach Unity.dmg
-mount
-ls -lh "/Volumes/Unity Download Assistant/"
-echo 'Installing Unity.pkg'
-sudo installer -verbose -allowUntrusted -dumplog -package Unity.pkg -target /
-hdiutil detach "/Volumes/Unity Download Assistant/"
-mount
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
