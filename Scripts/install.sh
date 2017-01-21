bf5a627ae53bb06a1fad50bf76ad431023655ec0
unknown <i7754737@PC34214.student.bournemouth.ac.uk>
2017-01-21 19:31:20 +0000

    facking idiot

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,12 +5,12 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 unity_url="http://download.unity3d.com/download_unity/960ebf59018a/UnityDownloadAssistant-5.3.5f1.dmg"
-echo 'Downloading from $unity_url: '
+echo "Downloading from $unity_url: "
 curl -o Unity.dmg "$unity_url"
-hdiutil attach "$(pwd)/Unity.pkg"
+hdiutil attach Unity.dmg
 mount
 ls -lh
 echo 'Installing Unity.pkg'
 # sudo installer -verbose -allowUntrusted -dumplog -package Unity.pkg -target /
-hdiutil detach "$(pwd)/Unity.pkg"
+hdiutil detach Unity.dmg
 mount
