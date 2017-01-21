7e7cd668d5e2fc2f45b2fb502801499c0ac82369
unknown <i7754737@PC34214.student.bournemouth.ac.uk>
2017-01-21 19:27:23 +0000

    Mount dmg as pkg

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,8 +6,11 @@
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 unity_url="http://download.unity3d.com/download_unity/960ebf59018a/UnityDownloadAssistant-5.3.5f1.dmg"
 echo 'Downloading from $unity_url: '
-curl -o Unity.pkg "$unity_url"
+curl -o Unity.dmg "$unity_url"
+hdiutil attach "$(pwd)/Unity.pkg"
+mount
 ls -lh
-file Unity.pkg
 echo 'Installing Unity.pkg'
-sudo installer -verbose -allowUntrusted -dumplog -package Unity.pkg -target /
+# sudo installer -verbose -allowUntrusted -dumplog -package Unity.pkg -target /
+hdiutil detach "$(pwd)/Unity.pkg"
+mount
